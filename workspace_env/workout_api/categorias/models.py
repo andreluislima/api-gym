413270ede40389c34cdc6f62fd33671e95548e32
andecodelima <andrelimag89@gmail.com>
2025-07-22 14:24:40 -0300

    alembic config

--- workspace_env/workout_api/categorias/models.py
+++ workspace_env/workout_api/categorias/models.py
@@ -1,13 +1,12 @@
-
+from __future__ import annotations
 from sqlalchemy import Integer, String
-from workspace_env.workout_api.atleta.models import AtletaModel
-from workspace_env.workout_api.contrib.models import BaseModel
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout_api.contrib.models import BaseModel
 
 class CategoriaModel(BaseModel):
-    
-    __tablename__ = 'categorias'
-    
-    pk_id: Mapped[int] = mapped_column(Integer, primary_key = True)
-    nome: Mapped[str] = mapped_column(String(50), nullable=False)
-    atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
+    __tablename__ = "categorias"
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    atletas: Mapped[list["AtletaModel"]] = relationship("AtletaModel", back_populates="categoria")
--- workspace_env/workout_api/centro_treinamento/models.py
+++ workspace_env/workout_api/centro_treinamento/models.py
@@ -1,16 +1,12 @@
-
+from __future__ import annotations
 from sqlalchemy import Integer, String
-from workspace_env.workout_api.atleta.models import AtletaModel
-from workspace_env.workout_api.contrib.models import BaseModel
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout_api.contrib.models import BaseModel
 
 class CentroTreinamentoModel(BaseModel):
-    
-    __tablename__ = 'centro_treinamento'
-    
-    pk_id: Mapped[int] = mapped_column(Integer, primary_key = True)
+    __tablename__ = "centros_treinamento"
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
-    endereco: Mapped[str] = mapped_column(String(60), nullable=False)
-    propietario: Mapped[str] = mapped_column(String(30), nullable=False)
-    
-    atleta: Mapped['AtletaModel'] = relationship(back_populates='centro_treinamento')
+
+    atletas: Mapped[list["AtletaModel"]] = relationship("AtletaModel", back_populates="centro_treinamento")
